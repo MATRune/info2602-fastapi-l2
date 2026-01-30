15fad4ccf813b13496a2b707be92d82135481efe
MATRune <malik.thomas.200531@gmail.com>
2026-01-30 14:39:11 -0400

    Update cli.py

--- app/cli.py
+++ app/cli.py
@@ -20,29 +20,70 @@ def initialize():
 
 @cli.command()
 def get_user(username:str):
+    with get_session() as db: # Get a connection to the database
+        user = db.exec(select(User).where(User.username == username)).first()
+        if not user:
+            print(f'{username} not found!')
+            return
+        print(user)
     # The code for task 5.1 goes here. Once implemented, remove the line below that says "pass"
-    pass
+    
 
 @cli.command()
 def get_all_users():
+    with get_session() as db:
+        all_users = db.exec(select(User)).all()
+        if not all_users:
+            print("No users found")
+        else:
+            for user in all_users:
+                print(user)
     # The code for task 5.2 goes here. Once implemented, remove the line below that says "pass"
-    pass
+    
 
 
 @cli.command()
 def change_email(username: str, new_email:str):
+    with get_session() as db: # Get a connection to the database
+        user = db.exec(select(User).where(User.username == username)).first()
+        if not user:
+            print(f'{username} not found! Unable to update email.')
+            return
+        user.email = new_email
+        db.add(user)
+        db.commit()
+        print(f"Updated {user.username}'s email to {user.email}")
     # The code for task 6 goes here. Once implemented, remove the line below that says "pass"
-    pass
+    
 
 @cli.command()
 def create_user(username: str, email:str, password: str):
+    with get_session() as db: # Get a connection to the database
+        newuser = User(username, email, password)
+        try:
+            db.add(newuser)
+            db.commit()
+        except IntegrityError as e:
+            db.rollback() #let the database undo any previous steps of a transaction
+            #print(e.orig) #optionally print the error raised by the database
+            print("Username or email already taken!") #give the user a useful message
+        else:
+            print(newuser) # print the newly created user
     # The code for task 7 goes here. Once implemented, remove the line below that says "pass"
-    pass
+    
 
 @cli.command()
 def delete_user(username: str):
+     with get_session() as db:
+        user = db.exec(select(User).where(User.username == username)).first()
+        if not user:
+            print(f'{username} not found! Unable to delete user.')
+            return
+        db.delete(user)
+        db.commit()
+        print(f'{username} deleted')
     # The code for task 8 goes here. Once implemented, remove the line below that says "pass"
-    pass
+    
 
 
 if __name__ == "__main__":
